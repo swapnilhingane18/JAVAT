ed09b1666c09e9865622495167b5b349d58cae27
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-18 23:48:40 +0530

    Add Variables.java - Learn data types: int, long, float, double, char, boolean, String

---
+++ Variables.java
@@ -0,0 +1,28 @@
+// Variables and Data Types in Java
+public class Variables {
+    public static void main(String args[]) {
+        // Integer types
+        int age = 21;
+        long population = 7800000000L;
+
+        // Decimal types
+        float price = 99.99f;
+        double pi = 3.14159265359;
+
+        // Character and Boolean
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        // String (not a primitive, but commonly used)
+        String name = "Swapnil";
+
+        System.out.println("=== Variables and Data Types ===");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("World Population: " + population);
+        System.out.println("Price: " + price);
+        System.out.println("Pi: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+    }
+}
