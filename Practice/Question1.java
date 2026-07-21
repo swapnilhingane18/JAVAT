b0283aa8a536537e56362eca94cb7059fa652e5a
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-21 22:16:45 +0530

    question 1

---
+++ Practice/Question1.java
@@ -0,0 +1,8 @@
+public class Question1{
+    public static void main(String[] args){
+
+        System.out.println("Swapnil");
+        System.out.println("At pune Dist Pune Tal Pune");
+
+    }
+    }
