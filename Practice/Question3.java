1fa73c484b25dcb16c7eedf276fda7fe9cd09d02
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-21 22:17:56 +0530

    que3

---
+++ Practice/Question3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Question3{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner (System.in);
+
+      
+
+        int p =sc.nextInt();
+        int r = sc.nextInt();
+        int t = sc.nextInt();
+        
+        double result = (p*r*t)/100;
+        System.out.println("simple intrest" + result );
+
+    }
+}
