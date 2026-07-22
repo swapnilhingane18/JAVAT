9cd89e7eeae91acd06fd38100d5bafd12814e70e
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-22 09:48:19 +0530

    swapping of 2 nums

---
+++ Practice/Question4.java
@@ -0,0 +1,28 @@
+//swap 2 numbers 
+
+public class Question4{
+    public static void main (String args[]){
+
+        int a=11;
+        int b=22;
+        int temp = 0;
+
+        temp=a;
+         a=b;
+         b=temp;
+         System.out.println(a);
+         System.out.println(b);
+
+         int x = 10;
+         int y = 20;
+
+         x=x+y;
+         y=x-y;
+         x=x-y;
+
+         System.out.println(x);
+         System.out.println(y);
+
+
+    }
+}
