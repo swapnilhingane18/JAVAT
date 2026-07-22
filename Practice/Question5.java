991920747019ab0db8a4d6d9c2ce8b695de82d66
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-22 10:24:43 +0530

    Q5 added

---
+++ Practice/Question5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Question5{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+        int a = c;
+        System.out.println(a);
+
+        int n = 65;
+        char v = (char) n;
+
+    }
+}
