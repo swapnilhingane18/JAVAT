403391ddba9adcd49ea7ab0fb7ba12ff0cbca4e5
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-22 09:31:41 +0530

    Compound intrest added

--- Practice/Question3.java
+++ Practice/Question3.java
@@ -9,9 +9,13 @@ public class Question3{
         int p =sc.nextInt();
         int r = sc.nextInt();
         int t = sc.nextInt();
+        int n = sc.nextInt();
         
         double result = (p*r*t)/100;
-        System.out.println("simple intrest" + result );
+        System.out.println("simple intrest"+" " + result );
+
+        double Compoundresult = p*Math.pow((1+r/(100*n)),n*t);
+        System.out.println("Compound Intrest is" + " " + Compoundresult);
 
     }
 }
