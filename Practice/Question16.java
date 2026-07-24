efc585f77b453db93dae44f137f1d4769d689728
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-24 11:22:44 +0530

    Reverse the number

---
+++ Practice/Question16.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Question16{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+
+        int rev=0;
+
+        while(num!=0){
+            int digit = num%10;
+            rev = rev*10 + digit;
+            num = num/10;
+        }
+        System.out.println(rev);
+    }
+}
