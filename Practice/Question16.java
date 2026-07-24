6c87f82ea2c922901db45017e84aa71686396045
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-24 11:38:58 +0530

    Palindrome check for number

--- Practice/Question16.java
+++ Practice/Question16.java
@@ -2,10 +2,11 @@ import java.util.*;
 public class Question16{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Enter a number");
         int num = sc.nextInt();
 
+        int original = num;
         int rev=0;
 
         while(num!=0){
@@ -13,6 +14,11 @@ public class Question16{
             rev = rev*10 + digit;
             num = num/10;
         }
-        System.out.println(rev);
+        if(rev==original){
+            System.out.println("NUmber is palindrome");
+        }
+        else{
+            System.out.println("Not A Palindrome");
+        }
     }
 }
