0b4ec6baa47a216a255d43484aafc93a6db5689a
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-07-31 12:22:24 +0530

    Functions and methods

---
+++ Practice/Sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sum{
+    public static void Sum(){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sumcalc= a+b;
+        System.out.println(sumcalc);
+    }
+    public static void main(String args[]){
+        Sum(); 
+    }
+}
