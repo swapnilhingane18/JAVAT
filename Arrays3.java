7a02fd794b3494124f6354e945cb3177eb60fb00
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-08-02 07:08:44 +0530

    Time optimized soln(used onlyy one loop)

--- Arrays3.java
+++ Arrays3.java
@@ -4,13 +4,16 @@ public class Arrays3{
     public static void main(String args[]){
         int[] arr = new int[5];
         Scanner sc = new Scanner(System.in);
+        int largest = arr[0];
+        int smallest =arr[0];
 
         for(int i=0;i<arr.length;i++){
             arr[i]= sc.nextInt();
-        }
-        int largest = arr[0];
-        int smallest =arr[0];
-        for(int i=0;i<arr.length;i++){
+
+            if(i==0){
+                largest = arr[i];
+                smallest =arr[i];
+            }
 
             if(largest<arr[i]){
                 largest=arr[i];
@@ -19,6 +22,7 @@ public class Arrays3{
                 smallest=arr[i];
             }
         }
+        
         System.out.println("The smallest is "+ smallest);
         System.out.println("The largest is "+ largest);
     }
