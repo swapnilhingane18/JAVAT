925ce1cf4d9785881a868962ce4c858afa4726ec
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-08-02 06:33:41 +0530

    Added codde for average

--- Arrays2.java
+++ Arrays2.java
@@ -12,6 +12,10 @@ public class Arrays2{
         for(int i=0;i<arr.length;i++){
             sum = sum + arr[i];
         }
+        System.out.println("The sum is ");
         System.out.println(sum);
+        int avg = sum/arr.length;
+        System.out.println("The average is ");
+        System.out.println(avg);
     }
 }
