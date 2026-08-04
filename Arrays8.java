7a1c2a258d6c598108816a402eb1d7e41777fad6
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-08-04 09:58:48 +0530

    feat: Add Arrays8 - count even numbers in an array

---
+++ Arrays8.java
@@ -0,0 +1,17 @@
+//count sum values in array
+import java.util.*;
+public class Arrays8{
+public static void main (String args[]){
+Scanner sc = new Scanner(System.in);
+int count=0;
+int[] arr = new int[5];
+for(int i=0;i<arr.length;i++){
+arr[i]=sc.nextInt();
+}
+for(int i=0;i<arr.length;i++){
+if(arr[i]%2==0){
+count ++;
+}
+}
+System.out.println(count);
+}}
