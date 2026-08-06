a1474c71877296f101c54d1da5950514cc9f043c
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-08-06 11:51:54 +0530

    style: format code in Question15.java

--- Practice/Question15.java
+++ Practice/Question15.java
@@ -1,16 +1,15 @@
 import java.util.*;
-public class Question15{
-    public static void main(String args[]){
+
+public class Question15 {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number ");
         int num = sc.nextInt();
-        int sum=0;
-        
+        int sum = 0;
 
-        for(int n= 1 ; n<=num ; n++){
+        for (int n = 1; n <= num; n++) {
             sum = sum + n;
         }
         System.out.println(sum);
-
     }
 }
