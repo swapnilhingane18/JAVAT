48b9e18602f27cb9d48241076d88b1fcfba93b55
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-08-09 18:48:29 +0530

    feat: add program to calculate sum of elements at even and odd indices (Arrays18.java)

---
+++ Arrays18.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Arrays18 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of a array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i % 2 == 0) {
+                count1 = count1 + arr[i];
+            } else {
+                count2 = count2 + arr[i];
+            }
+        }
+        System.out.println("sum of even num count " + count1);
+        System.out.println("sum of odd num count " + count2);
+    }
+}
