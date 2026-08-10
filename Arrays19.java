2628accbc7a38800cc4eff595af3672c1a25764a
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-08-10 21:54:04 +0530

    feat(Arrays19): add user prompts for array size and element input

--- Arrays19.java
+++ Arrays19.java
@@ -3,8 +3,10 @@ import java.util.*;
 public class Arrays19 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
         int n = sc.nextInt();
         int[] arr = new int[n];
+        System.out.println("Enter the elements of array");
 
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
