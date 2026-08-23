f9bf9ddf9000511b3503cdf286ed236debe31fc6
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-08-23 11:30:00 +0530

    Add Demo16: Java class and variable scope demonstration

---
+++ Demo16.java
@@ -0,0 +1,10 @@
+public class Demo16{
+    public static void main(String[]args){
+        int a;
+        System.out.println(" "+ a);
+        class China{
+            int b;
+            System.out.println(b);
+        }
+    }
+}
