0aada915c27872121435b2e5f43b0f361ea09444
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-08-26 11:30:00 +0530

    Add Q18: total loan repayment calculator using EMI and months

---
+++ Q18.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Q18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int emi = sc.nextInt();
+        int months = sc.nextInt();
+
+        System.out.println(emi * months);
+    }
+}
