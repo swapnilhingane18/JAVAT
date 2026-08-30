3340b56e149ec240972ea1e48e77c43ee9d403c3
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-08-30 11:00:00 +0530

    Add Switch1.java - switch statement practice

---
+++ Practice/Switch1.java
@@ -0,0 +1,17 @@
+public class Switch1{
+    public static void main(String[] args){
+        int n=1;
+        switch(n){
+            case 1:System.out.println("Monday");break;
+            case 2:System.out.println("Tuesday");break;
+            case 3:System.out.println("Wensday");break;
+            case 4:System.out.println("Thursday");break;
+            case 5:System.out.println("Friday");break;
+            case 6:System.out.println("Saturday");break;
+            case 7:System.out.println("Sunday");break;
+            
+
+             
+        }
+    }
+}
