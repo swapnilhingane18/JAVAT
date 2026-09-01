543f27ed0c53f16e0f487b7e6b3dd0cd20f97f06
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-01 11:00:00 +0530

    Add Ternary1.java - ternary operator practice

---
+++ Practice/Ternary1.java
@@ -0,0 +1,7 @@
+public class Ternary1{
+    public static void main(String[] args) {
+        int marks=51;
+        String sub =(marks>20 && marks<50)?"pass":"fail";
+        System.out.println(sub);
+    }
+}
