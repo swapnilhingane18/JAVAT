045ffce89ade58a70c206d44c8e4d4c7c32af0bb
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-01 21:51:06 +0530

    addition of data types

---
+++ Practice/Demo3.java
@@ -0,0 +1,10 @@
+public class Demo3{
+    public static void main(String[] args) {
+        int a=10;
+        String b ="Swap";
+        char c='a';
+        int d=20;
+
+        System.out.println(a+ " "+b+" "+" "+c+" "+d);
+    }
+}
