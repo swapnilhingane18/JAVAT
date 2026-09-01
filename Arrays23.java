790bc73ef59a570cc359bb13ea5b36a4393b9294
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-01 22:15:01 +0530

    arr copy to another arr

---
+++ Arrays23.java
@@ -0,0 +1,11 @@
+public class Arrays23{
+public static void main(String [] args){
+int[] arr={1,2,3,4,5,6};
+int[] arr2=new int[arr.length];
+
+for(int i=0;i<arr.length;i++){
+
+arr2[i]=arr[i];
+System.out.println(arr[i]);
+}}
+}
