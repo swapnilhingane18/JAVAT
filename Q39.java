cb91d057944c7a1d5a1f6804ed566db1e6cf5692
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-02 10:30:00 +0530

    Add Q39: Java practice problem - calculations

---
+++ Q39.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Q39 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int start_time = sc.nextInt();
+        int end_time = sc.nextInt();
+
+        System.out.println(end_time - start_time);
+    }
+}
