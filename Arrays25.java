d908508b60611f6cce1d1601c665162312015afd
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-03 23:35:33 +0530

    Add Arrays25: 1D array initialization and sequential element printing using for loop

---
+++ Arrays25.java
@@ -0,0 +1,14 @@
+public class Arrays25{
+    public static void main(String[] args) {
+        int[] nums=new int[6];
+
+    for(int i=0;i<nums.length;i++){
+        nums[i]=i;
+        System.out.print(nums[i]+" ");
+        
+    }
+    
+
+    }
+    
+}
