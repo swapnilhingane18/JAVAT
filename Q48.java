734e56a3acf8d911b640e9d8d649d336335033a0
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-04 11:20:00 +0530

    Add Q48: Java practice problem - calculations

---
+++ Q48.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Q48 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int dist = sc.nextInt();
+        int speed = sc.nextInt();
+        
+        System.out.println(dist/speed);
+    }
+}
