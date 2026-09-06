a59588a0f7b05a794654892fdcb8cd2276fb9087
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-06 10:30:00 +0530

    Add Q52: Java practice problem - calculations

---
+++ Q52.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Q52 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int units = sc.nextInt();
+        int rate = sc.nextInt();
+
+        System.out.println(units * rate);
+    }
+}
