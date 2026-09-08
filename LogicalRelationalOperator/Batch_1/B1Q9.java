463fe0fb286967056cf1e78ede539037b4186344
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-08 12:00:00 +0530

    Add B1Q9: age eligibility check using >= 18 comparison

---
+++ LogicalRelationalOperator/Batch_1/B1Q9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class B1Q9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        System.out.println(age >= 18);
+
+        sc.close();
+    }
+}
