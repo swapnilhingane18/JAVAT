8a04dc9abf790f89738d5d80441a1e8e9ca9a54c
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-08 10:30:00 +0530

    Add B1Q3: check if student passed using marks >= 35 comparison

---
+++ LogicalRelationalOperator/Batch_1/B1Q3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class B1Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+
+        System.out.println(marks >= 35);
+
+        sc.close();
+
+    }
+}
