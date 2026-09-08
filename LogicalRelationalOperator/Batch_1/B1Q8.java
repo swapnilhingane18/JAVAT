d42cee4b951d059d48f504d25c2aca82ffedaf2b
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-08 11:45:00 +0530

    Add B1Q8: check if amount exceeds 500 using relational operator

---
+++ LogicalRelationalOperator/Batch_1/B1Q8.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class B1Q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int amount = sc.nextInt();
+        System.out.println(amount > 500);
+
+        sc.close();
+    }
+}
