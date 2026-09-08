3a97d43fd36e00bd5681a58326308d927faa62f6
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-08 12:15:00 +0530

    Add B1Q10: check if number is greater and divisible using logical AND operator

---
+++ LogicalRelationalOperator/Batch_1/B1Q10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class B1Q10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a > b && a % b == 0);
+
+        sc.close();
+    }
+}
