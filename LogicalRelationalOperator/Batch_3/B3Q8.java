befc3227e902e6961fa921ecc59197b5d0fd72d7
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-12 10:00:00 +0530

    Add B3Q8: check if integer division equals 2.5 using relational operator

---
+++ LogicalRelationalOperator/Batch_3/B3Q8.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class B3Q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt(), y = sc.nextInt();
+
+        System.out.println(x / y == 2.5);
+
+        sc.close();
+    }
+}
