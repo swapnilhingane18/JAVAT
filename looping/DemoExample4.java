305aa22300a0f1fbb962d26324727cb0636edba4
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-13 11:00:00 +0530

    Add DemoExample4: variable declaration scope demonstration inside unbraced if statement

---
+++ looping/DemoExample4.java
@@ -0,0 +1,12 @@
+class DemoExample4{
+    public static void main(String[] args) {
+        System.out.println("Starts");
+
+        if(true)
+           int a = 123;
+
+        System.out.println("byeee");
+
+        System.out.println("Ends");
+    }
+}
