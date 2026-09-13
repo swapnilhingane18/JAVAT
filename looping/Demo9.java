c9b2c2ef2ddae11e176fc275ddb4084935cdf7a5
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-13 10:30:00 +0530

    Add Demo9: divisibility check with prioritized condition and return statements

---
+++ looping/Demo9.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Demo9{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the input : ");
+
+        int n = sc.nextInt();
+
+        if(n % 2 == 0 && n % 5 == 0){
+            System.out.println("HiTwoHiFive");
+            return;
+        }
+
+        if(n % 2 == 0){
+            System.out.println("HiTwo");
+            return;
+        }
+
+        if(n % 5 == 0){
+            System.out.println("HiFive");
+            return;
+        }
+
+    }
+}
