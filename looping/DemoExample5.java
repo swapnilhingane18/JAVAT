77dba6298f02b0348ebb370155f959dc840f784f
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-13 11:15:00 +0530

    Add DemoExample5: execution flow and unbraced if statement block behavior

---
+++ looping/DemoExample5.java
@@ -0,0 +1,11 @@
+class DemoExample5{
+    public static void main(String[] args) {
+        
+        System.out.println("Starts");
+        if(!true)
+            System.out.println("hello");
+            System.out.println("byeee");
+        System.out.println("Ends");
+    }
+}
+
