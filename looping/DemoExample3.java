c494a9399f388c225c78e85a1c6fe980690805b6
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-13 10:45:00 +0530

    Add DemoExample3: conditional variable initialization with unbraced if statement

---
+++ looping/DemoExample3.java
@@ -0,0 +1,14 @@
+class DemoExample3{
+    public static void main(String[] args) {
+        System.out.println("Starts");
+
+        int a;
+
+        if(true)
+            a = 123;
+
+        System.out.println("byeee");
+
+        System.out.println("Ends");
+    }
+}
