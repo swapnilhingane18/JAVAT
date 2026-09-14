59bed18a0e819a8f93080b181584282db9d3b1b1
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-14 10:20:00 +0530

    Add Q62: trace execution flow when if condition is false using negation

---
+++ Q62.java
@@ -0,0 +1,12 @@
+class Q62{
+    public static void main(String[] args) {
+        System.out.println("starts");
+        int num =10;
+        if(!true){
+            System.out.println("hello");
+        }
+        System.out.println(num);
+        System.out.println("ends");
+    
+    }
+}
