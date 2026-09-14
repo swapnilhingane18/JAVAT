e0033d80642a9dc18b367d6d3379deb65ef45ca4
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-14 11:00:00 +0530

    Add Q64: compile-time error when uninitialized variable is accessed outside if block

---
+++ Q64.java
@@ -0,0 +1,13 @@
+class Q64{
+    public static void main(String[] args) {
+        System.out.println("starts");
+        int num ;
+        if(true){
+            System.out.println("hello");
+            num=20;
+        }
+        System.out.println(num);
+        System.out.println("ends");
+    
+    }
+}
