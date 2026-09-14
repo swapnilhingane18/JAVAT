cfab2d650dad15f49829e7fde835082ecd7ac354
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-14 11:20:00 +0530

    Add Q65: variable retains original value when if block with false condition is skipped

---
+++ Q65.java
@@ -0,0 +1,13 @@
+class Q65{
+    public static void main(String[] args) {
+        System.out.println("starts");
+        int num =10;
+        if(!true){
+            System.out.println("hello");
+            num=20;
+        }
+        System.out.println(num);
+        System.out.println("ends");
+    
+    }
+}
