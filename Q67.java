0c162d5a32aabe4e798470c6d2d0da5c445d58d9
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-15 10:20:00 +0530

    Add Q67: inner block skipped when if condition is false, outer variable unaffected

---
+++ Q67.java
@@ -0,0 +1,16 @@
+class Q67{
+    public static void main(String[] args) {
+        System.out.println("starts");
+        int num =10;
+        if(!true){
+            int num2=20;
+            System.out.println("hello");
+            System.out.println(num);
+            System.out.println(num2);
+        }
+        System.out.println(num);
+        //System.out.println(num2);
+        System.out.println("ends");
+    
+    }
+}
