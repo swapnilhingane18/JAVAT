331c8691b2471e5ef5bb4a897ad8893be1c35cc4
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-15 11:00:00 +0530

    Add Q69: string comparison logic for job application response with rejection message

---
+++ Q69.java
@@ -0,0 +1,13 @@
+class Q69{
+    public static void main(String[] args) {
+        System.out.println("Applies for capgemini");
+        System.out.println("Applies for capgemini");
+        System.out.println("Applies for capgemini");
+        System.out.println("Applies for capgemini");
+        String resp="NO";
+        if(resp!="YES"){
+            System.out.println("Sorry u are rejected");
+        }
+        System.out.println("Ends");
+    }
+}
