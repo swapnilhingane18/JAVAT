6b971897ca2b8a66e55cd4fcef11d79c7e25493a
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-16 10:20:00 +0530

    Add Q72: swap two integers using temp variable and arithmetic without temp

---
+++ Q72.java
@@ -0,0 +1,16 @@
+public class Q72{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a+ " " +b);
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a+" "+b);
+    }
+}
