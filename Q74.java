8e7baa5f113f31b7fe19dea4c7b6ba323a7283cf
Swapnil Hingane <swapnilhingane9290@gmail.com>
2026-09-16 11:00:00 +0530

    Add Q74: compute product of loop iterations using separate accumulator variable

---
+++ Q74.java
@@ -0,0 +1,11 @@
+class Q74 {
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 0;
+
+        for (int i = 1; i < n; i++) {
+            m = n * i;
+        }
+        System.out.println(m);
+    }
+}
